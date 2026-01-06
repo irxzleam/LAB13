e55c33bd2334d0d8096b3e848fd27dda77cc9354
irxzleam <maelzxri@gmail.com>
2026-01-06 16:02:14 +0700

    1completed

--- lab13_1.cpp
+++ lab13_1.cpp
@@ -19,3 +19,36 @@ int main(){
     cout << "Min = " << B[5];
     return 0;
 }
+
+void stat(const double a[],int n,double b[])
+{
+
+    for(int i = 0;i<n;i++) b[0] += a[i]/n;
+
+
+    double devsum=0;
+    for(int j = 0;j<n;j++) devsum += pow(a[j]-b[0],2);
+    b[1] = sqrt(devsum/n);
+
+
+    double kun =1.0;
+    for(int k = 0;k<n;k++) kun *= a[k];
+    b[2] = pow(kun,1.0/n);
+
+
+    double harn = 0;
+    for(int l = 0;l<n;l++) harn += 1.0/a[l];
+    b[3] = n/harn;
+
+       
+    int m=1;
+    double max= a[0];
+    double min = a[0];
+    while (m<n){
+    if(a[m]>max) max = a[m];
+    if(a[m]<min) min = a[m];
+    m++;
+    }
+    b[4] = max;
+    b[5] = min;
+}
